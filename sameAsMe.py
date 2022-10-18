c3fcfd2d3dacdce331ee895af495ee15ffec49cf
andrewshowers <andrew.showers@hope.edu>
2022-10-18 18:48:17 -0400

    Initial commit

---
+++ sameAsMe.py
@@ -0,0 +1,9 @@
+myFav = 24
+theirFav = int(input('please enter your favorite number'))
+if myFav > theirFav:
+    print('your number is too small')
+elif myFav == theirFav:
+    print('We must be BFF\'s')
+else:
+    print('your number is too big')
+
